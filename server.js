de0ea7ec57571db614e7d12ded6104a2a4b3b5ce
Innocent Rukundo <rukundoinnocent27@gmail.com>
2025-11-07 02:21:54 -0500

    second commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const MongoClient = require('mongodb').MongoClient
 let db, collection;
 
 
-const url = "mongodb+srv://rukundoinnocent27_db_user:zIThklssDxlf0LwN@cluster0.1gbsbrs.mongodb.net/book?retryWrites=true&w=majority";
+const url = "mongodb+srv://rukundoinnocent27_db_user:yNb53pus74D2iowR@cluster0.1gbsbrs.mongodb.net/book?retryWrites=true&w=majority";
 const dbName = "book";
 
 app.listen(4001, () => {
